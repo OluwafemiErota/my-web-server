9c16d1f3da1a461d33193ba148cbfdd97861c80c
OluwafemiErota <erota.oluwafemi@gmail.com>
2024-07-02 14:44:52 -0500

    Add root route to handle /

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Root route
+app.get('/', (req, res) => {
+  res.send('Welcome to the API. Use /api/hello?visitor_name=YourName to get a personalized greeting.');
+});
+
 app.get('/api/hello', async (req, res) => {
   const visitorName = req.query.visitor_name || 'Guest';
   let clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
